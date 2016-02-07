c4fa0d1f6c49c8a58526dc30e09d2897ccaa12d6
harryliuhao <harryliuhao@hotmail.com>
2016-02-06 20:54:44 -0500

    Hao's submission for week 1 assignment

---
+++ plot1.R
@@ -0,0 +1,19 @@
+##copy data files into the same folder of working directory
+setwd("./r_projects/t4assignments")
+getwd()
+
+df<-read.csv("household_power_consumption.txt", sep=";", header=T)
+df$Date<-as.Date(as.character(df$Date),format = "%d/%m/%Y")
+df<-subset(df,Date>=as.Date("2/1/2007",,format = "%m/%d/%Y") & Date<=as.Date("2/2/2007",,format = "%m/%d/%Y"))
+df$Global_active_power<-as.numeric(as.character(df$Global_active_power))
+df$Sub_metering_1<-as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2<-as.numeric(as.character(df$Sub_metering_2))
+df$Voltage<-as.numeric(as.character(df$Voltage))
+df$Global_reactive_power<-as.numeric(as.character(df$Global_reactive_power))
+df$dt_value<-as.POSIXct(paste(df$Date, df$Time), format="%Y-%m-%d %H:%M:%S")
+
+##plot 1
+par(mfrow=c(1,1))
+hist(df$Global_active_power, col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png",width = 480, height = 480)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,27 @@
+##copy data files into the same folder of working directory
+setwd("./r_projects/t4assignments")
+getwd()
+
+df<-read.csv("household_power_consumption.txt", sep=";", header=T)
+df$Date<-as.Date(as.character(df$Date),format = "%d/%m/%Y")
+df<-subset(df,Date>=as.Date("2/1/2007",,format = "%m/%d/%Y") & Date<=as.Date("2/2/2007",,format = "%m/%d/%Y"))
+df$Global_active_power<-as.numeric(as.character(df$Global_active_power))
+df$Sub_metering_1<-as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2<-as.numeric(as.character(df$Sub_metering_2))
+df$Voltage<-as.numeric(as.character(df$Voltage))
+df$Global_reactive_power<-as.numeric(as.character(df$Global_reactive_power))
+df$dt_value<-as.POSIXct(paste(df$Date, df$Time), format="%Y-%m-%d %H:%M:%S")
+
+##plot 4
+par(mfrow=c(2,2))
+par(mar=c(4,4,1,1))
+plot(df$dt_value,df$Global_active_power, type="l",xlab="",ylab="Global Active Power")
+plot(df$dt_value,df$Voltage, type="l", xlab="Datetime", ylab="Voltage")
+
+plot(df$dt_value,df$Sub_metering_1, type="l",col="black",xlab="",ylab="Energy sub metering")
+lines(df$dt_value,df$Sub_metering_2, type="l",col="red")
+lines(df$dt_value,df$Sub_metering_3, type="l",col="blue")
+legend("top", lty=c(1,1,1), col=c("black","red","blue"), legend=names(df[,7:9]),xjust=-1,cex=0.6,y.intersp=1,bty="n")
+plot(df$dt_value,df$Global_reactive_power, type="l",xlab="Datetime", ylab="Global_reactive_power")
+dev.copy(png, file="plot4.png",width = 480, height = 480)
+dev.off()
